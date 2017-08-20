7547fa15ef9f0363d2e5a4348cb9b9b60a8f2640
Omkar Prabhu <omkkarprabuinfinity@gmail.com>
2017-08-20 16:24:02 +0530

    Card redesign

--- angular/controller/projects.js
+++ angular/controller/projects.js
@@ -10,21 +10,34 @@ var showProjects = function ($scope) {
 
 };
 
-
+// all projects
 var projects = [
     {
-        'title': 'NITK-Login',
-        'description': 'Android Application to auto-sign into NITK-NET Wifi',
-        'link': 'https://github.com/MO-DevTeam/NITK-Login',
-        'img': 'images/NITK-Login.png'
+        title: 'NITK-Login',
+        description: 'Android Application to auto-sign into NITK-NET Wifi',
+        link: 'https://github.com/MO-DevTeam/NITK-Login',
+        img : 'images/NITKLogin.png'
+
     },
     {
         title: 'SPlay',
         description: 'Dark Theme Android Music Player',
         link: 'https://www.github.com/omkarprabhu-98/SPlay',
-        'img': 'images/SPlay.png'
-
+        img: 'images/SPlay.png'
+    },
+    {
+        title: 'Rapid 54',
+        description: 'Android game to ',
+        link: 'https://www.github.com/MO-DevTeam/Rapid54',
+        img: 'images/Rapid54.png'
+    },
+    {
+        title: 'KannadaApp',
+        description: 'Android Application for learning basics of Kannada Language',
+        link: 'https://www.github.com/omkarprabhu-98/KannadaApp',
+        img: 'images/KannadaApp.png'
     }
+
 ];
 
 
