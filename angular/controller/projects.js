6e22f2ced36ceaf6e4daf4173682dabc8b5ad020
Omkar Prabhu <omkkarprabuinfinity@gmail.com>
2017-12-31 16:34:33 +0530

    Contact page started

--- angular/controller/projects.js
+++ angular/controller/projects.js
@@ -22,13 +22,13 @@ var projects = [
         title: 'SPlay',
         description: 'Dark Theme Android Music Player',
         link: 'https://www.github.com/omkarprabhu-98/SPlay',
-        tags: ['Android', 'Java', 'Android-Xml']
+        tags: ['Android']
     },
     {
         title: 'Rapid 54',
         description: 'Android game to click on tiles from 1-54 in ascending order given a time limit',
         link: 'https://www.github.com/MO-DevTeam/Rapid54',
-        tags: ['Android', 'Java', 'Android-Xml']
+        tags: ['Android']
     },
     {
         title: 'ModifiedEnigmaMachine',
@@ -40,13 +40,13 @@ var projects = [
         title: 'GuessWhat',
         description: 'Q&A Android app for which a user has to answer a question based on a scrambled image. It uses Firebase as a backend Edit\n',
         link: 'https://github.com/omkarprabhu-98/GuessWhat',
-        tags: ['Android', 'Java', 'Android-Xml', 'Firebase']
+        tags: ['Android', 'Firebase', 'Material-Design']
     },
     {
         title: 'BasicChat',
         description: 'Android Chat application in which signed-in users can post text and image messages. It uses Firebase for backend Edit\n',
         link: 'https://github.com/omkarprabhu-98/BasicChat',
-        tags: ['Android', 'Java', 'Android-Xml', 'Firebase']
+        tags: ['Android', 'Firebase']
     },
     {
         title: 'TxtRec',
@@ -58,7 +58,7 @@ var projects = [
         title: 'NITK-Login',
         description: 'Android Application to auto-sign into NITK-NET Wifi',
         link: 'https://github.com/MO-DevTeam/NITK-Login',
-        tags: ['Android', 'Java', 'Android-Xml']
+        tags: ['Android']
     }
 
 
