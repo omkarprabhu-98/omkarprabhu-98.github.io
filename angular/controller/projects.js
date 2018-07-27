21d485ee953aecc30758d6ac9ce080062eeedc73
Omkar Prabhu <omkkarprabuinfinity@gmail.com>
2018-07-27 19:14:46 +0530

    project list fixed

--- angular/controller/projects.js
+++ angular/controller/projects.js
@@ -37,22 +37,16 @@ var projects = [
         tags: ['Android', 'Firebase', 'Material-Design']
     },
     {
-        title: 'Modified Enigma Machine',
-        description: 'CO-202 Mini Project. A modified implementation of the Enigma machine used in World War II using digital electronics with computer aided design',
-        link: 'https://github.com/omkarprabhu-98/ModifiedEnigmaMachine',
-        tags: ['Verilog-HDL', 'Logisim', 'Digital-Design']
-    },
-    {
-        title: 'Modified Enigma Machine',
-        description: 'CO-202 Mini Project. A modified implementation of the Enigma machine used in World War II using digital electronics with computer aided design',
-        link: 'https://github.com/omkarprabhu-98/ModifiedEnigmaMachine',
-        tags: ['Verilog-HDL', 'Logisim', 'Digital-Design']
-    },
-    {
         title: 'MapReduce Jobs for Hadoop',
         description: 'Collection of examples for jobs in python for Hadoop using MapReduce programming model (On Going)',
         link: 'https://github.com/omkarprabhu-98/mapreduce-jobs-for-hadoop',
         tags: ['Hadoop', 'MapReduce', 'luigi']
+    },
+    {
+        title: 'Modified Enigma Machine',
+        description: 'CO-202 Mini Project. A modified implementation of the Enigma machine used in World War II using digital electronics with computer aided design',
+        link: 'https://github.com/omkarprabhu-98/ModifiedEnigmaMachine',
+        tags: ['Verilog-HDL', 'Logisim', 'Digital-Design']
     },
     {
         title: 'SPlay',
