613644dfb29ec33851d20116599b5988a48ebae9
Omkar Prabhu <omkkarprabuinfinity@gmail.com>
2019-01-23 18:23:25 +0530

    basic routing for nav pills

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'omkar-srinivas-prabhu';
+	navVar;
+	title = 'omkar-srinivas-prabhu';
+	
+	constructor(private router: Router) {
+		this.navVar = 1;
+	}
+	
+	navToggle(i) {
+		this.navVar = i;
+	}
+	redirect(path) {
+		this.router.navigate([path]);
+	}
 }
